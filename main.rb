2811c7f156fc12e85f65543ee62bc47cf3436cdd
tetetratra <44367208+tetetratra@users.noreply.github.com>
2021-12-29 01:00:44 +0900

    コールスタック方式に改修中2

--- main.rb
+++ main.rb
@@ -2,9 +2,11 @@
 class Lisp
     Fn = Struct.new(:args, :codes)
 
+    Closure = Struct.new(:fn, :env)
+
     Env = Struct.new(:parent, :bindings) do
-      def add(name, value)
-        Env[parent, { **bindings, name => value }]
+      def add(hash)
+        Env[parent, { **bindings, **hash }]
       end
 
       def find(name)
@@ -15,21 +17,21 @@ class Lisp
       end
     end
 
-    Flame = Struct.new(:fn, :line, :env) do
+    Frame = Struct.new(:fn, :line, :env) do
       def current_code
         fn.codes[line]
       end
 
       def next
-        Flame[fn, line + 1, env]
+        Frame[fn, line + 1, env]
       end
 
       def last_line?
-        fn.codes.size - 1 == line
+        fn.codes.size - 1 <= line
       end
 
       def update_env(next_env)
-        Flame[fn, line, next_env]
+        Frame[fn, line, next_env]
       end
     end
 
@@ -38,8 +40,12 @@ class Lisp
       parsed = parse("(-> () #{src})")
       @function_table = construct_function_table(parsed)
       root_fn = @function_table.first
-      root_env = Env[nil, {}]
-      root_frame = Flame[root_fn, 0, root_env]
+      root_env = Env[nil, {
+        :+ => ->(args, env){ args.inject(:+) },
+        :- => ->(args, env){ args.first - args[1..].inject(:+) },
+        :p => ->(args, env){ p args }
+      }]
+      root_frame = Frame[root_fn, 0, root_env]
       eval([root_frame], nil)
     end
 
@@ -85,19 +91,17 @@ class Lisp
     end
 
     def eval(cs, prev_value) # 再帰NG
-      print "\ncs => "
-      pp cs
-
       frame = cs.last
       code = frame.current_code
       env = frame.env
 
-      calced_value, next_env = calc(code, env)
+      # prev_value は前回のframeの計算の結果
+      # calced_value は今回の計算の結果
+      calced_value, next_env, new_frame = calc(code, env, prev_value)
 
-      print 'calced_value => '
-      pp calced_value
-
-      next_cs = if frame.last_line?
+      next_cs = if new_frame
+                  [*cs[..-2], frame.next.update_env(next_env), new_frame]
+                elsif frame.last_line?
                   cs[..-2]
                 else
                   [*cs[..-2], frame.next.update_env(next_env)]
@@ -105,12 +109,9 @@ class Lisp
       eval(next_cs, calced_value) unless next_cs.empty?
     end
 
-    def calc(code, env) # 再帰OK
-      print 'code => '
-      p code
-      print 'env => '
-      p env
-
+    def calc(code, env, prev_value = nil) # 再帰OK
+      puts '-------------'
+      pp code
       case code
       in Integer
         [code, env]
@@ -118,19 +119,38 @@ class Lisp
         [env.find(sym), env]
       in [:'=', name, value]
         calced_value = calc(value, env)[0]
-        [calced_value, env.add(name, calced_value)]
-      in [Symbol => sym, *args]
+        print 'calced_value =>'
+        pp calced_value
+        [name, env.add({ name => calced_value })]
+      in [:'->', [*args], *codes]
+        fn = @function_table.find { |f| f.args == args && f.codes == codes }
+        closure = Closure[fn, env]
+        [closure, env]
+      in [Symbol => name, *args]
+        value = env.find(name)
+        calc([value, *args], env)
+      in [Proc => pro, *args]
+        calced_args = args.map { |a| calc(a, env)[0] }
+        [pro.(calced_args, env), env]
+      in [Closure => closure, *args]
+        calced_args = args.map { |a| calc(a, env)[0] }
+        new_frame = Frame[
+          closure.fn,
+          0,
+          closure.env.add(closure.fn.args.zip(calced_args).to_h)
+        ]
+        # 関数呼び出しなのでcsにpush
+        [prev_value, env, new_frame]
+      in nil
+        [nil, nil]
       end
     end
   end
 end
 
-lisp = Lisp.run(<<~LISP)
-  1
-  (= x 2)
-  (+ x x)
-  x
+Lisp.run(<<~LISP)
+  (= g (-> (a) b))
+  (= r (g 150))
+  (p r)
 LISP
-# pp lisp.send(:construct_function_table, )
-
 
