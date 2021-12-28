71036ebb6a0260959a86190d20a70b76ffb70b80
tetetratra <44367208+tetetratra@users.noreply.github.com>
2021-12-28 22:08:55 +0900

    コールスタック方式に改修中 & メモを追加

--- main.rb
+++ main.rb
@@ -1,152 +1,136 @@
 
 class Lisp
-  Env = Struct.new(:parent, :bindings) do
-    def add(key, value)
-      bindings[key] = value unless add_rec(key, value)
-      self
+    Fn = Struct.new(:args, :codes)
+
+    Env = Struct.new(:parent, :bindings) do
+      def add(name, value)
+        Env[parent, { **bindings, name => value }]
+      end
+
+      def find(name)
+        find_r = ->(name, env) do
+          env.bindings[name] || find_r.(name, env.parent) if env
+        end
+        find_r.(name, self)
+      end
     end
 
-    def add_rec(key, value)
-      if bindings[key]
-        bindings[key] = value
-        true
-      else
-        if parent.nil?
-          false
+    Flame = Struct.new(:fn, :line, :env) do
+      def current_code
+        fn.codes[line]
+      end
+
+      def next
+        Flame[fn, line + 1, env]
+      end
+
+      def last_line?
+        fn.codes.size - 1 == line
+      end
+
+      def update_env(next_env)
+        Flame[fn, line, next_env]
+      end
+    end
+
+  class << self
+    def run(src)
+      parsed = parse("(-> () #{src})")
+      @function_table = construct_function_table(parsed)
+      root_fn = @function_table.first
+      root_env = Env[nil, {}]
+      root_frame = Flame[root_fn, 0, root_env]
+      eval([root_frame], nil)
+    end
+
+    def parse(str)
+      regex = /\(|\)|[\w\d\-+=*%_@^~<>?$&|!]+/
+      tokens = str.gsub(/#.*/, '').gsub(/\s+/, ' ').scan(regex).map do |token|
+        case token
+        when /^\d+$/
+          token.to_i
         else
-          parent.add_rec(key, value)
+          token.to_sym
         end
       end
-    end
-  end
-
-  Closure = Struct.new(:args, :code, :env)
-
-  Code = Struct.new(:exp, :line)
-
-  def initialize(code)
-    @exp = parse(code)
-    @env = Env.new(nil, {
-      :+ => ->(args){ args[0] + args[1] },
-      :- => ->(args){ args[0] - args[1] },
-      :* => ->(args){ args[0] * args[1] },
-      :'==' => ->(args){ args[0] == args[1] },
-      :'!=' => ->(args){ args[0] == args[1] },
-      :p => ->(args){ p args[0] }
-    })
-  end
-
-  def run
-    eval(@exp, @env)
-  end
-
-  private
-
-  def parse(str)
-    regex = /\(|\)|[\w\d\-+=*%_@^~<>?$&|!]+/
-    tokens = str.gsub(/#.*/, '').gsub(/\s+/, ' ').scan(regex).map do |token|
-      case token
-      when /^\d+$/
-        token.to_i
-      else
-        token.to_sym
-      end
-    end
-    parsed = [tokens.shift]
-    until tokens.empty?
-      parsed <<
-        case s = tokens.shift
-        when :')'
-          poped = [:')']
-          until poped in [:'(', *rest, :')']
-            poped = [parsed.pop, *poped]
+      parsed = [tokens.shift]
+      until tokens.empty?
+        parsed <<
+          case s = tokens.shift
+          when :')'
+            poped = [:')']
+            until poped in [:'(', *rest, :')']
+              poped = [parsed.pop, *poped]
+            end
+            poped[1..-2]
+          else
+            s
           end
-          poped[1..-2]
-        else
-          s
-        end
-    end
-    parsed.first
-  end
-
-  def eval(exp, env)
-    case exp
-    in []
-      [nil, env]
-    in Symbol
-      [find_value(exp, env), env]
-    in Integer
-      [exp, env]
-    in [:code | :~, *rest]
-      eval([Code.new(rest, 0), *rest], env)
-    in [:'=', a, b]
-      b_value = eval(b, env)[0]
-      [nil, env.add(a, b_value)]
-    in [:'->', [*vars], code]
-      [Closure.new(vars, code, env), env]
-    in [:if, *rest]
-      case rest
-      in [a,b]
-        if eval(a, env)[0]
-          eval(b, env)
-        else
-          [nil, env]
-        end
-      in [a,b,c]
-        if eval(a, env)[0]
-          eval(b, env)
-        else
-          eval(c, env)
-        end
       end
-    in [Code => code, *args]
-      # pp exp
-      case args
-      in [current]
-        eval(current, env)
-      in [current, *rest]
-        eval([Code.new(rest, code.line + 1), *rest], eval(current, env)[1])
+      parsed
+    end
+
+    def construct_function_table(exp)
+      case exp
+      in [:'->', [*args], *codes]
+        [
+          Fn.new(args, codes),
+          *codes.map { |c| construct_function_table(c) }.flatten
+        ]
+      in [*codes]
+        codes.map { |c| construct_function_table(c) }.flatten
+      else
+        []
       end
-    # in [:callcc, closure_node]
-      # TODO: 未実装
-      # eval(exp, env, call_stack) にする？
-      # Callcc構造体に保存する
-      # code組み込み関数でevalするときに、
-    # in [Callcc => cc, *rest]
-      # TODO: 未実装
-    in [Proc => pr, *rest] # (組み込みの)関数の呼び出し
-      args = rest.map { |t| eval(t, env)[0] }
-      [pr.call(args), env]
-    in [Closure => cl, *rest] # (ユーザー定義の)クロージャの呼び出し
-      new_env = Env.new(
-        cl.env,
-        cl.args.zip(rest.map { |r| eval(r, env)[0] }).to_h
-      )
-      eval(cl.code, new_env)
-    in [Array => first, *rest]
-      f = eval(first, env)[0]
-      eval([f, *rest], env)
-    in [Symbol => first, *rest] # (p (f))は関数を呼び出ている。(p f)は関数を扱っているだけ
-      f = find_value(first, env)
-      eval([f, *rest], env)
     end
-  end
 
-  def find_value(name, env)
-    v = find_value_rec(name, env)
-    if v.nil?
-      pp env
-      raise "`#{name}` not found"
+    def eval(cs, prev_value) # 再帰NG
+      print "\ncs => "
+      pp cs
+
+      frame = cs.last
+      code = frame.current_code
+      env = frame.env
+
+      calced_value, next_env = calc(code, env)
+
+      print 'calced_value => '
+      pp calced_value
+
+      next_cs = if frame.last_line?
+                  cs[..-2]
+                else
+                  [*cs[..-2], frame.next.update_env(next_env)]
+                end
+      eval(next_cs, calced_value) unless next_cs.empty?
     end
-    v
-  end
 
-  def find_value_rec(name, env)
-    return nil if env.nil?
-    env.bindings[name] || find_value_rec(name, env.parent)
+    def calc(code, env) # 再帰OK
+      print 'code => '
+      p code
+      print 'env => '
+      p env
+
+      case code
+      in Integer
+        [code, env]
+      in Symbol => sym
+        [env.find(sym), env]
+      in [:'=', name, value]
+        calced_value = calc(value, env)[0]
+        [calced_value, env.add(name, calced_value)]
+      in [Symbol => sym, *args]
+      end
+    end
   end
 end
 
-Lisp.new(<<~LISP).run
+lisp = Lisp.run(<<~LISP)
+  1
+  (= x 2)
+  (+ x x)
+  x
 LISP
+# pp lisp.send(:construct_function_table, )
+
 
