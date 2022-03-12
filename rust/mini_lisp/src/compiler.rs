5b651017d410e36ae7e73d2cde300b8ac2a9f0c9
tetetratra <44367208+tetetratra@users.noreply.github.com>
2022-03-12 18:51:39 +0900

    whileなどのコンパイル

--- rust/mini_lisp/src/compiler.rs
+++ rust/mini_lisp/src/compiler.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use super::parser::Ast;
 
 // #[derive(Debug)]
@@ -10,7 +11,13 @@ trait AsRef<T: ?Sized> {
 pub fn compile(ast: Ast) -> Vec<String> {
     match ast {
         Ast::S(s) => {
-            vec![s] // TODO: 文字,数値,変数 に分ける
+            if Regex::new(r#"^-?\d+$"#).unwrap().is_match(s.as_str()) {
+                vec![format!("int@{}", s)]
+            } else if Regex::new(r#"^"(.*)"$"#).unwrap().is_match(s.as_str()) {
+                vec![format!("str@{}", s)]
+            } else {
+                vec![format!("get@{}", s)]
+            }
         }
         Ast::A(ast_vec) => {
             let first = ast_vec[0].clone();
@@ -27,11 +34,28 @@ pub fn compile(ast: Ast) -> Vec<String> {
                     [
                         if_compiled,
                         vec![format!("jumpif@{}", else_compiled_len + 1)],
-                        then_compiled,
-                        vec![format!("jump@{}", then_compiled_len)],
                         else_compiled,
+                        vec![format!("jump@{}", then_compiled_len)],
+                        then_compiled
                     ]
                     .concat()
+                }
+                Ast::S(s) if s == "while".to_string() => {
+                    let cond = ast_vec[1].clone();
+                    let compiled_cond = compile(cond);
+                    let compiled_cond_len = compiled_cond.len();
+
+                    let statements: Vec<Ast> = ast_vec.clone().drain(2..).collect();
+                    let compiled_statements: Vec<String> = statements.into_iter().flat_map(compile).collect();
+                    let compiled_statements_len = compiled_statements.len();
+
+                    vec![
+                      compiled_cond,
+                      vec![format!("jumpunless@{}", compiled_statements_len + 1)],
+                      compiled_statements,
+                      vec![format!("jump@{}", -(2 + compiled_statements_len as i32 + compiled_cond_len as i32))],
+                      vec!["get@nil".to_string()]
+                    ].concat()
                 }
                 ast => { // マッチしなかったシンボル or ベクタ
                     let mut ast_vec_c = ast_vec.clone();
