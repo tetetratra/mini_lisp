01026a1b293f8c68af552394ece823976e32282f
tetetratra <44367208+tetetratra@users.noreply.github.com>
2022-03-12 11:39:22 +0900

    parserをファイルに分割

---
+++ rust/mini_lisp/src/parser.rs
@@ -0,0 +1,81 @@
+use regex::Regex;
+use std::fmt;
+
+#[derive(Clone)]
+enum Parsed {
+    S(String),
+    P(Vec<Parsed>),
+}
+
+impl Parsed {
+    fn inspect(&self, depth: i32) {
+        let mut indent = "".to_string();
+        for _ in 0..depth {
+            indent.push_str("  ");
+        }
+        match self {
+            Parsed::S(s) => {
+                print!("{}", s);
+            }
+            Parsed::P(pv) => {
+                print!("\n{}(", indent);
+                for (i, p) in pv.into_iter().enumerate() {
+                    p.inspect(depth + 1);
+                    if i != pv.len() - 1 {
+                        print!(" ")
+                    };
+                }
+                print!(")");
+            }
+        };
+    }
+}
+
+impl fmt::Debug for Parsed {
+    fn fmt(&self, _f: &mut fmt::Formatter) -> fmt::Result {
+        self.inspect(0);
+        Ok(())
+    }
+}
+
+pub fn parse(raw_code: String) {
+    let raw_code_without_comment = Regex::new(r"[#;].*")
+        .unwrap()
+        .replace_all(raw_code.as_str(), "")
+        .into_owned();
+    let code = Regex::new(r"\s+")
+        .unwrap()
+        .replace_all(raw_code_without_comment.as_str(), " ");
+
+    let regex = Regex::new(r#"\(|\)|[\w\d\-+=*%_@^~<>?$&|!]+|".+?""#).unwrap();
+    let mut tokens = regex.find_iter(&code).map(|m| m.as_str().to_string());
+
+    let mut parsed: Vec<Parsed> = vec![];
+    parsed.push(Parsed::S(tokens.next().unwrap()));
+    let tokens_rest: Vec<String> = tokens.collect();
+
+    for token in tokens_rest {
+        let element = match token.as_str() {
+            ")" => {
+                let mut poped: Vec<Parsed> = vec![Parsed::S(")".to_string())];
+                loop {
+                    match [poped.first(), poped.last()] {
+                        [Some(Parsed::S(fst)), Some(Parsed::S(lst))]
+                            if fst.as_str() == "(" && lst.as_str() == ")" =>
+                        {
+                            break Parsed::P(poped.drain(1..=poped.len() - 2).collect())
+                        }
+                        _ => {
+                            poped.insert(0, parsed.pop().unwrap());
+                        }
+                    }
+                }
+            }
+            _ => Parsed::S(token),
+        };
+        parsed.push(element);
+    }
+    for p in parsed {
+        print!("{:?}", p);
+    }
+}
