b400fdf4ef410d7fe8ac21e5b0404d930cd7b30f
tetetratra <44367208+tetetratra@users.noreply.github.com>
2022-03-13 01:12:28 +0900

    直接ファイルを動かせるようにした(ruby)

--- ruby/mini_lisp/lib/mini_lisp.rb
+++ ruby/mini_lisp/lib/mini_lisp.rb
@@ -3,17 +3,26 @@
 require_relative 'mini_lisp/version'
 require_relative 'mini_lisp/parser'
 require_relative 'mini_lisp/compiler'
+require_relative 'mini_lisp/vm'
 require_relative 'mini_lisp/evaluator'
 
 module MiniLisp
   class Error < StandardError; end
 
   def self.run(src)
-    # puts src if $debug
+    puts src if $debug
     parsed = Parser.parse(src)
-    # pp parsed if $debug
+    pp parsed if $debug
     code_table = Compiler.compile([:~, *parsed])
-    # pp code_table if $debug
+    pp code_table if $debug
     Evaluator.exec(code_table)
   end
+end
+
+# bundle exec ruby -W0 lib/mini_lisp.rb ../../if.mlisp -g -d
+if $PROGRAM_NAME == __FILE__
+  $debug = !!ARGV.delete('-d')
+  $gc_every_time = !!ARGV.delete('-g')
+  src = File.open(ARGV.first).read
+  MiniLisp.run(src)
 end
