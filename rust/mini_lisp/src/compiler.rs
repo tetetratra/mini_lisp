fecfad7406f721b2e585e23146da32c46a1e917d
tetetratra <44367208+tetetratra@users.noreply.github.com>
2022-03-17 09:02:55 +0900

    コンパイラのリファクタリング2

--- rust/mini_lisp/src/compiler.rs
+++ rust/mini_lisp/src/compiler.rs
@@ -2,141 +2,153 @@ use super::parser::Ast;
 use regex::Regex;
 
 pub fn compile(ast: Ast, code_table: Vec<Vec<String>>) -> (Vec<String>, Vec<Vec<String>>) {
-    let mut code_table = code_table.clone();
-    let code = match ast {
-        Ast::S(s) => {
-            if Regex::new(r#"^-?\d+$"#).unwrap().is_match(s.as_str()) {
-                vec![format!("int@{}", s)]
-            } else if Regex::new(r#"^"(.*)"$"#).unwrap().is_match(s.as_str()) {
-                vec![format!("str@{}", s)]
+    match ast {
+        Ast::S(string) => {
+            if Regex::new(r#"^-?\d+$"#).unwrap().is_match(string.as_str()) {
+                (vec![format!("int@{}", string)], code_table)
+            } else if Regex::new(r#"^"(.*)"$"#).unwrap().is_match(string.as_str()) {
+                (vec![format!("str@{}", string)], code_table)
             } else {
-                vec![format!("get@{}", s)]
+                (vec![format!("get@{}", string)], code_table)
             }
         }
         Ast::A(ast_vec) => {
-            let mut ast_vec_iter = ast_vec.into_iter();
+            let mut ast_iter = ast_vec.into_iter();
 
-            let first_ast = ast_vec_iter.next().unwrap();
-            let mut rest_asts = ast_vec_iter;
+            let first_ast = ast_iter.next().unwrap();
+            let mut rest_asts = ast_iter;
 
             match first_ast {
-                Ast::S(s) if s == "~".to_string() => rest_asts
-                    .flat_map(|a| {
-                        let (c, next_code_table) = compile(a, code_table.clone());
-                        code_table = next_code_table;
-                        c
-                    })
-                    .collect(),
-                Ast::S(s) if s == "if".to_string() => {
-                    let (if_compiled, code_table_if) =
-                        compile(rest_asts.next().unwrap(), code_table.clone());
-                    code_table = code_table_if;
-                    let (then_compiled, code_table_then) =
-                        compile(rest_asts.next().unwrap(), code_table.clone());
-                    code_table = code_table_then;
-                    let (else_compiled, code_table_else) =
-                        compile(rest_asts.next().unwrap(), code_table.clone());
-                    code_table = code_table_else;
-                    let else_compiled_len = else_compiled.len();
-                    let then_compiled_len = then_compiled.len();
-                    [
-                        if_compiled,
-                        vec![format!("jumpif@{}", else_compiled_len + 1)],
-                        else_compiled,
-                        vec![format!("jump@{}", then_compiled_len)],
-                        then_compiled,
-                    ]
-                    .concat()
-                }
-                Ast::S(s) if s == "while".to_string() => {
-                    let cond = rest_asts.next().unwrap();
-                    let (compiled_cond, code_table_cond) = compile(cond, code_table.clone());
-                    code_table = code_table_cond;
-                    let compiled_cond_len = compiled_cond.len();
-
-                    let statements: Vec<Ast> = rest_asts.collect();
-                    let compiled_statements: Vec<String> = statements
-                        .into_iter()
-                        .flat_map(|s| {
-                            let (code_s, code_table_s) = compile(s, code_table.clone());
-                            code_table = code_table_s;
-                            code_s
+                Ast::S(string) => match string.as_str() {
+                    "~" => {
+                        rest_asts.fold((vec![], code_table), |(memo_code, memo_code_table), ast| {
+                            let (code, new_code_table) = compile(ast, memo_code_table);
+                            (vec![memo_code, code].concat(), new_code_table)
                         })
-                        .collect();
-                    let compiled_statements_len = compiled_statements.len();
-
-                    vec![
-                        compiled_cond,
-                        vec![format!("jumpunless@{}", compiled_statements_len + 1)],
-                        compiled_statements,
-                        vec![format!(
-                            "jump@{}",
-                            -(2 + compiled_statements_len as i32 + compiled_cond_len as i32)
-                        )],
-                        vec!["get@nil".to_string()],
-                    ]
-                    .concat()
-                }
-                Ast::S(s) if s == "=".to_string() => {
-                    let variable_name = if let Ast::S(vn) = rest_asts.next().unwrap() {
-                        vn
-                    } else {
-                        panic!()
-                    };
-                    let (code_val, code_table_val) =
-                        compile(Ast::A(rest_asts.collect()), code_table.clone());
-                    code_table = code_table_val;
-                    vec![code_val, vec![format!("set@{}", variable_name)]].concat()
-                }
-                Ast::S(s) if s == "->".to_string() => {
-                    let args_ast = rest_asts.next().unwrap();
-                    let args: String = if let Ast::A(a) = args_ast {
-                        a.into_iter()
-                            .map(|aa| match aa {
-                                Ast::S(string) => string,
-                                Ast::A(_) => {
-                                    panic!()
-                                }
-                            })
-                            .collect::<Vec<String>>()
-                            .join(",")
-                    } else {
-                        panic!()
-                    };
-                    let codes_ast = rest_asts;
-                    let new_code = codes_ast
-                        .flat_map(|c| {
-                            let (c, next_code_table) = compile(c, code_table.clone());
-                            code_table = next_code_table;
-                            c
-                        })
-                        .collect();
-
-                    let closure_index = code_table.len();
-                    code_table.push(new_code);
-                    vec![format!("closure@{}@{}", closure_index, args)]
-                }
-                _ => {
-                    // (method ...) | ((...) ...)
-                    let args = rest_asts;
-                    let args_len = args.len();
-
-                    let (code_ast, code_table_ast) = compile(first_ast, code_table.clone());
-                    code_table = code_table_ast;
-                    [
-                        args.flat_map(|a| {
-                            let (c, next_code_table) = compile(a, code_table.clone());
-                            code_table = next_code_table;
-                            c
-                        })
-                        .collect(),
-                        code_ast,
-                        vec![format!("send@{:?}", args_len)],
-                    ]
-                    .concat()
-                }
+                    }
+                    "if" => {
+                        let (if_compiled, code_table) =
+                            compile(rest_asts.next().unwrap(), code_table);
+                        let (then_compiled, code_table) =
+                            compile(rest_asts.next().unwrap(), code_table);
+                        let (else_compiled, code_table) =
+                            compile(rest_asts.next().unwrap(), code_table);
+                        (
+                            [
+                                if_compiled,
+                                vec![format!("jumpif@{}", else_compiled.len() + 1)],
+                                else_compiled,
+                                vec![format!("jump@{}", then_compiled.len())],
+                                then_compiled,
+                            ]
+                            .concat(),
+                            code_table,
+                        )
+                    }
+                    "while" => {
+                        let (compiled_cond, code_table) =
+                            compile(rest_asts.next().unwrap(), code_table);
+                        let compiled_cond_len = compiled_cond.len();
+                        let (compiled_statements, code_table) = rest_asts.fold(
+                            (vec![], code_table),
+                            |(memo_code, memo_code_table), ast| {
+                                let (code, new_code_table) = compile(ast, memo_code_table);
+                                (vec![memo_code, code].concat(), new_code_table)
+                            },
+                        );
+                        let compiled_statements_len = compiled_statements.len();
+                        (
+                            vec![
+                                compiled_cond,
+                                vec![format!("jumpunless@{}", compiled_statements.len() + 1)],
+                                compiled_statements,
+                                vec![format!(
+                                    "jump@{}",
+                                    -(2 + compiled_statements_len as i32
+                                        + compiled_cond_len as i32)
+                                )],
+                                vec!["get@nil".to_string()],
+                            ]
+                            .concat(),
+                            code_table,
+                        )
+                    }
+                    "=" => {
+                        if let Ast::S(variable_name) = rest_asts.next().unwrap() {
+                            let (code_val, code_table) =
+                                compile(Ast::A(rest_asts.collect()), code_table);
+                            (
+                                vec![code_val, vec![format!("set@{}", variable_name)]].concat(),
+                                code_table,
+                            )
+                        } else {
+                            panic!()
+                        }
+                    }
+                    "->" => {
+                        if let Ast::A(args) = rest_asts.next().unwrap() {
+                            let joined_args = args
+                                .into_iter()
+                                .map(|arg| match arg {
+                                    Ast::S(string) => string,
+                                    Ast::A(_) => {
+                                        panic!()
+                                    }
+                                })
+                                .collect::<Vec<String>>()
+                                .join(",");
+                            let (new_code, code_table) = rest_asts.fold(
+                                (vec![], code_table),
+                                |(memo_code, memo_code_table), ast| {
+                                    let (new_code, code_table) = compile(ast, memo_code_table);
+                                    (vec![memo_code, new_code].concat(), code_table)
+                                },
+                            );
+                            let closure_index = code_table.len();
+                            (
+                                vec![format!("closure@{}@{}", closure_index, joined_args)],
+                                vec![code_table, vec![new_code]].concat(),
+                            )
+                        } else {
+                            panic!()
+                        }
+                    }
+                    _ => compile_other(
+                        Ast::A(std::iter::once(Ast::S(string)).chain(rest_asts).collect()),
+                        code_table,
+                    ),
+                },
+                ast_a @ Ast::A(_) => compile_other(
+                    Ast::A(std::iter::once(ast_a).chain(rest_asts).collect()),
+                    code_table,
+                ),
             }
         }
-    };
-    (code, code_table)
+    }
+}
+
+fn compile_other(ast: Ast, code_table: Vec<Vec<String>>) -> (Vec<String>, Vec<Vec<String>>) {
+    // (method ...) | ((...) ...)
+    match ast {
+        Ast::A(ast_vec) => {
+            let mut ast_iter = ast_vec.into_iter();
+            let first_ast = ast_iter.next().unwrap();
+            let rest_asts = ast_iter;
+
+            let args_len = rest_asts.len();
+            let (code_ast, code_table) = compile(first_ast, code_table);
+            let (code, code_table) =
+                rest_asts.fold((vec![], code_table), |(memo_code, memo_code_table), ast| {
+                    let (new_code, code_table) = compile(ast, memo_code_table);
+                    (vec![memo_code, new_code].concat(), code_table)
+                });
+            (
+                [code, code_ast, vec![format!("send@{:?}", args_len)]].concat(),
+                code_table,
+            )
+        }
+        Ast::S(_) => {
+            panic!()
+        }
+    }
 }
