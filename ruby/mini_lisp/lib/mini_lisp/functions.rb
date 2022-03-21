f0be63a8205a202a0ae1a4ec3de1f4496b7bb0c6
tetetratra <44367208+tetetratra@users.noreply.github.com>
2022-03-21 18:24:45 +0900

    コード整理

--- ruby/mini_lisp/lib/mini_lisp/functions.rb
+++ ruby/mini_lisp/lib/mini_lisp/functions.rb
@@ -5,15 +5,13 @@ module MiniLisp
 
   Functions = {
     :nil => Value::Nil[],
-    :nil? => Fn { args[0] == Value::Nil[] ? Value::True[] : Value::False[] },
     :true => Value::True[],
     :false => Value::False[],
+    :nil? => Fn { args[0] == Value::Nil[] ? Value::True[] : Value::False[] },
     :list => Fn { args.reverse.reduce(Value::Nil[]) { |cons, a| Value::Cons[a, cons] } },
     :cons => Fn { Value::Cons[args[0], args[1]] },
     :car => Fn { args[0].car },
     :cdr => Fn { args[0].cdr },
-    :callcc => :callcc,
-    :gc => :gc,
     :'+' => Fn { Value::Num[args[0].to_ruby + args[1].to_ruby] },
     :'-' => Fn { Value::Num[args[0].to_ruby - args[1].to_ruby] },
     :'==' => Fn { args[0].to_ruby == args[1].to_ruby ? Value::True[] : Value::False[] },
@@ -22,5 +20,7 @@ module MiniLisp
     :p => Fn { p args.first },
     :sleep => Fn { sleep(args.first); args.first },
     :stack_frames_size => Fn { vm.stack_frames.size },
+    :callcc => :callcc,
+    :gc => :gc,
   }
 end
