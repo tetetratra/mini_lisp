22b69d2f3951ca589e1c3c5be29a3f254e7e26c9
tetetratra <44367208+tetetratra@users.noreply.github.com>
2022-08-25 01:55:46 +0900

    [ruby] VMを構造体からクラスに

--- ruby/mini_lisp/lib/mini_lisp/vm.rb
+++ ruby/mini_lisp/lib/mini_lisp/vm.rb
@@ -5,24 +5,35 @@ require 'rainbow/refinement'
 using Rainbow
 
 module MiniLisp
-  VM = Struct.new(:stack_frame_num, :stack_frames) do
+  class VM
+    attr_accessor :stack_frame_num, :stack_frames
+
+    def initialize(stack_frame_num, stack_frames)
+      @stack_frame_num = stack_frame_num
+      @stack_frames = stack_frames
+    end
+
+    class << self
+      alias [] new
+    end
+
     def change_stack_frame_num(n)
       VM[
         n,
-        stack_frames
+        @stack_frames
       ].freeze
     end
 
     def current_stack_frame
-      stack_frames[stack_frame_num]
+      @stack_frames[@stack_frame_num]
     end
 
     def current_stack_frame_call_parent
-      stack_frames[current_stack_frame.call_parent_num]
+      @stack_frames[current_stack_frame.call_parent_num]
     end
 
     def current_stack_frame_env_parent
-      stack_frames[current_stack_frame.env_parent_num]
+      @stack_frames[current_stack_frame.env_parent_num]
     end
 
     def current_stack_frame_finish?(code_table)
@@ -30,35 +41,35 @@ module MiniLisp
     end
 
     def current_stack_frame_find_env(name)
-      current_stack_frame.find_env(name, stack_frames)
+      current_stack_frame.find_env(name, @stack_frames)
     end
 
     def current_stack_frame_update_env(name, value)
-      target_stack_frame_num = loop.reduce(stack_frame_num) do |num, _|
-        stack_frame = stack_frames[num]
+      target_stack_frame_num = loop.reduce(@stack_frame_num) do |num, _|
+        stack_frame = @stack_frames[num]
         env_parent_num = stack_frame.env_parent_num
 
         if stack_frame.env.key?(name)
           break num
         elsif env_parent_num.nil?
           # 根まで辿ってもなかったら現在のスタックフレームの環境を使う
-          break stack_frame_num
+          break @stack_frame_num
         else
           env_parent_num
         end
       end
 
       VM[
-        stack_frame_num,
+        @stack_frame_num,
         {
-          **stack_frames.except(target_stack_frame_num),
+          **@stack_frames.except(target_stack_frame_num),
           target_stack_frame_num => StackFrame[
-            stack_frames[target_stack_frame_num].stack,
-            { **stack_frames[target_stack_frame_num].env, name => value },
-            stack_frames[target_stack_frame_num].line_num,
-            stack_frames[target_stack_frame_num].call_parent_num,
-            stack_frames[target_stack_frame_num].env_parent_num,
-            stack_frames[target_stack_frame_num].code_table_num
+            @stack_frames[target_stack_frame_num].stack,
+            { **@stack_frames[target_stack_frame_num].env, name => value },
+            @stack_frames[target_stack_frame_num].line_num,
+            @stack_frames[target_stack_frame_num].call_parent_num,
+            @stack_frames[target_stack_frame_num].env_parent_num,
+            @stack_frames[target_stack_frame_num].code_table_num
           ]
         }
       ].freeze
@@ -66,10 +77,10 @@ module MiniLisp
 
     def current_stack_frame_stack_push(value)
       VM[
-        stack_frame_num,
+        @stack_frame_num,
         {
-          **stack_frames.except(stack_frame_num),
-          stack_frame_num => StackFrame[
+          **@stack_frames.except(@stack_frame_num),
+          @stack_frame_num => StackFrame[
             [*current_stack_frame.stack, value],
             current_stack_frame.env,
             current_stack_frame.line_num,
@@ -83,10 +94,10 @@ module MiniLisp
 
     def current_stack_frame_stack_pop
       vm = VM[
-        stack_frame_num,
+        @stack_frame_num,
         {
-          **stack_frames.except(stack_frame_num),
-          stack_frame_num => StackFrame[
+          **@stack_frames.except(@stack_frame_num),
+          @stack_frame_num => StackFrame[
             current_stack_frame.stack[..-2],
             current_stack_frame.env,
             current_stack_frame.line_num,
@@ -101,10 +112,10 @@ module MiniLisp
 
     def current_stack_frame_line_num_add(n)
       VM[
-        stack_frame_num,
+        @stack_frame_num,
         {
-          **stack_frames.except(stack_frame_num),
-          stack_frame_num => StackFrame[
+          **@stack_frames.except(@stack_frame_num),
+          @stack_frame_num => StackFrame[
             current_stack_frame.stack,
             current_stack_frame.env,
             current_stack_frame.line_num + n,
@@ -117,29 +128,29 @@ module MiniLisp
     end
 
     def available_stack_frame_num
-      (0..).find { |n| !stack_frames.keys.include?(n) }
+      (0..).find { |n| !@stack_frames.keys.include?(n) }
     end
 
     def insert_stack_frame(n, stack_frame)
       VM[
-        stack_frame_num,
+        @stack_frame_num,
         {
-          **stack_frames,
+          **@stack_frames,
           n => stack_frame
         }
       ].freeze
     end
 
     def gc
-      keep = stack_frames.transform_values { |_sf| false }
+      keep = @stack_frames.transform_values { |_sf| false }
 
-      keep[stack_frame_num] = true
-      stack = [stack_frame_num]
+      keep[@stack_frame_num] = true
+      stack = [@stack_frame_num]
 
       until stack.empty?
         poped_stack_frame_num = stack.pop
-        stack_frame = stack_frames[poped_stack_frame_num]
-        raise "gc bug: stack_frames[#{poped_stack_frame_num}] is nil" if stack_frame.nil?
+        stack_frame = @stack_frames[poped_stack_frame_num]
+        raise "gc bug: @stack_frames[#{poped_stack_frame_num}] is nil" if stack_frame.nil?
 
         if (cpn = stack_frame.call_parent_num) && !keep[cpn]
           stack << cpn
@@ -177,8 +188,8 @@ module MiniLisp
       puts "droped: #{keep.reject { _2 }.keys.to_set.inspect[/{.*}/]}".cyan if $debug && keep.reject { _2 }.any?
 
       VM[
-        stack_frame_num,
-        stack_frames.select { |num, _sf| keep[num] }
+        @stack_frame_num,
+        @stack_frames.select { |num, _sf| keep[num] }
       ]
     end
   end
